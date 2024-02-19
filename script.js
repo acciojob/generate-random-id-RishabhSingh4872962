24ad02a734fe3f37e21f0171feb99f6614e6d43e
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-19 21:04:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function makeid(l) {
   // write your code here
-}
+	let arr="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	let id=""
+
+	for (let i = 0; i < l; i++) {
+		id=id+arr[Math.floor(Math.random() *arr.length -1)]
+	}
+
+	
+	
+	return id;
+} 
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
